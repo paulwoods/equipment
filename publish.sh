71d15c1637fa2e18b880635a56a42d1a75afb375
mrpaulwoods <mr.paul.woods@gmail.com>
2025-12-29 18:36:07 -0600

    revert(release): downgrade version to 0.1.5 and update related files
    - Reverted `package.json` and `package-lock.json` version back to 0.1.5.
    - Updated `publish.sh` to reflect Docker image tagging for version 0.1.5.
    - Adjusted `CHANGELOG.md` to remove 0.1.6 entry and restore 0.1.5 as the latest version.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 docker build -t equipment-management .
 docker tag equipment-management paulwoods/equipment-management:latest
-docker tag equipment-management paulwoods/equipment-management:0.1.6
+docker tag equipment-management paulwoods/equipment-management:0.1.5
 docker push paulwoods/equipment-management:latest
-docker push paulwoods/equipment-management:0.1.6
+docker push paulwoods/equipment-management:0.1.5
