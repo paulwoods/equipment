ce3538b0a507b17ece8bf6c7a3eaa198d173cec5
mrpaulwoods <mr.paul.woods@gmail.com>
2025-12-29 18:14:58 -0600

    chore(release): bump version to 0.1.4 and update changelog
    - Updated `package.json` and `package-lock.json` version to 0.1.4.
    - Adjusted `publish.sh` to tag and push Docker images with version 0.1.4.
    - Added a new entry in `CHANGELOG.md` for version 0.1.4.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 docker build -t equipment-management .
 docker tag equipment-management paulwoods/equipment-management:latest
-docker tag equipment-management paulwoods/equipment-management:0.1.3
+docker tag equipment-management paulwoods/equipment-management:0.1.4
 docker push paulwoods/equipment-management:latest
-docker push paulwoods/equipment-management:0.1.3
+docker push paulwoods/equipment-management:0.1.4
